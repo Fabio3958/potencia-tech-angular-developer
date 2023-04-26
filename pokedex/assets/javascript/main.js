d949781ca6a00f457f4c4fc681732146ea20534c
fabio <fabioevangelista_@hotmail.com>
2023-04-25 21:48:37 -0300

    pokedex - manipulando o resultado da requisicao atraves de uma promise

--- pokedex/assets/javascript/main.js
+++ pokedex/assets/javascript/main.js
@@ -3,7 +3,11 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url).then(function (response){
-    console.log(response);
+    response.json().then(function (responseBody){
+        console.log(responseBody);
+    });
+}).catch(function (error){
+    console.log(error);
+}).finally(function (){
+    console.log('Requisição concluída');
 });
-
-console.log(20);
